3daec6339736f2895dcd8cbfbf92f057b38783e3
DomwareApps <domware.apps@gmail.com>
2019-12-23 11:47:45 +0100

    Updates

---
+++ precache-manifest.4cfb2abb45092b74f1a5586d71bf458e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8665dae5cdb86db29d583ce5a8aac49",
+    "url": "/pdf-generator-frontend/index.html"
+  },
+  {
+    "revision": "cb3235782a0100bb2fb7",
+    "url": "/pdf-generator-frontend/static/css/main.7fd7d100.chunk.css"
+  },
+  {
+    "revision": "4b47209914d41f093c11",
+    "url": "/pdf-generator-frontend/static/js/2.371f9f69.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pdf-generator-frontend/static/js/2.371f9f69.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cb3235782a0100bb2fb7",
+    "url": "/pdf-generator-frontend/static/js/main.cd543c86.chunk.js"
+  },
+  {
+    "revision": "eb4a9fe8fdf5438dcafe",
+    "url": "/pdf-generator-frontend/static/js/runtime-main.fdac4c38.js"
+  }
+]);
